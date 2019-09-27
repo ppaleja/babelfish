587b6898915fac74d9d0ce020c5272aa23975ea8
ppaleja <46587956+ppaleja@users.noreply.github.com>
2019-09-26 20:20:15 -0700

    Update App.js

--- babelfish/App.js
+++ babelfish/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { 
+
+import {
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
   AsyncStorage,
- } from 'react-native';
+} from 'react-native';
+
 import * as Permissions from 'expo-permissions';
+
 import { Audio } from 'expo-av';
 
 const ASYNC_STORAGE_AUDIO_KEY = 'ASYNC_STORAGE_AUDIO_KEY';
@@ -14,64 +17,112 @@ const ASYNC_STORAGE_AUDIO_KEY = 'ASYNC_STORAGE_AUDIO_KEY';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      recording: false,
+      isRecording: false,
+      recording: new Audio.Recording(),
       permissionGranted: false,
-    }
+    };
   }
 
   handleButtonPress = async () => {
-    const { recording, permissionGranted } = this.state;
-    if(!permissionGranted) {
+    const { isRecording, permissionGranted } = this.state;
+
+    if (!permissionGranted) {
       this.alertIfRemoteNotificationsDisabledAsync();
+
       return;
     }
-    
-    if(recording) {
+
+    if (isRecording) {
       this.stopRecording();
     } else {
       this.startRecording();
     }
-  }
+  };
+
+  playAudio = async (audio) => {
+    const { recording } = this.state;
+    const soundObject = await recording.createNewLoadedSoundAsync();
+    alert(soundObject);
+    //alert('brh');
+    //alert('brh' + require(recording.getURI()));
+    try {
+      //const src = recording.getURI();
+      //alert(src);
+      await soundObject.playAsync();
+      // Your sound is playing!
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  stopRecording = async () => {
+    //alert('stop recording');
+    const { isRecording, recording } = this.state;
+    try {
+      await recording.stopAndUnloadAsync();
+    } catch (error) {
+      alert(error);
+    }
+
+    this.playAudio(recording);
+    // Need logic to send audio recording to dragon board here
+
+    this.setState({
+      isRecording: false,
+      recording: new Audio.Recording(),
+    });
+  };
 
   startRecording = async () => {
-    const recording = new Audio.Recording();
+    const { recording } = this.state;
     try {
-      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      await recording.prepareToRecordAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+
       await recording.startAsync();
+
       // You are now recording!
     } catch (error) {
-      // An error occurred!
+      alert('audio error: ' + error);
     }
-  }
+    //alert('start Recording')
+    this.setState(prevState => ({
+      isRecording: true,
+    }));
+  };
 
   alertIfRemoteNotificationsDisabledAsync = async () => {
-    const { status, permissions } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+    const { status, permissions } = await Permissions.askAsync(
+      Permissions.AUDIO_RECORDING
+    );
+
     if (status !== 'granted') {
       alert('Hey! You need audio permission to use my app.');
     } else {
       this.setState({
         permissionGranted: true,
-      })
+      });
     }
-  }
+  };
 
   async componentDidMount() {
     this.alertIfRemoteNotificationsDisabledAsync();
-    try {
-      const audio = 
-    }
   }
 
   render() {
-    const { recording } = this.state;
-    const buttonText = recording ? 'stop' : 'start';
-    return(
+    //alert('rendering');
+    const { isRecording } = this.state;
+
+    const buttonText = isRecording ? 'stop' : 'start';
+
+    return (
       <View style={styles.container}>
         <TouchableOpacity
           style={styles.button}
-          onPress={this.handleButtonPress}
-        >
+          onPress={this.handleButtonPress}>
           <Text>{buttonText}</Text>
         </TouchableOpacity>
       </View>
@@ -89,6 +140,6 @@ const styles = StyleSheet.create({
   button: {
     alignItems: 'center',
     backgroundColor: '#DDDDDD',
-    padding: 10
+    padding: 10,
   },
 });
